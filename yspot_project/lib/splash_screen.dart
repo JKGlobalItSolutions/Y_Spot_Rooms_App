eb19ebac520807a42d4c1ce0bcaac51dfee5c3e9
jkglobalitsolution8 <jkitsolutions8@gmail.com>
2024-05-17 17:38:07 +0530

    Today Changes
    1. Terminated the old project and created a new project, fixed the bugs in android studio while connecting to the git.
    2.working on new yspot project for now created a splash screen only and reviewed a flutter page about creating adaptive ui and animations

---
+++ yspot_project/lib/splash_screen.dart
@@ -0,0 +1,60 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  final Widget child;
+  const SplashScreen({super.key, required this.child});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+
+    Future.delayed(const Duration(seconds: 5)).then((value) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => widget.child),
+              (route) => false);
+    });
+    super.initState();
+
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                bottom: 40.0,
+                left: 20,
+                right: 20,
+              ),
+              child: const Text(
+                "Hello, traveler! let's find you the perfect room for your next adventure!!",
+                style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
